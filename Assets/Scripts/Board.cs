845a9543fd6350bdbd114c6bfde0403c4c073d16
Mehael <mehael@mail.ru>
2017-07-29 14:15:51 +1000

    Add board generator and moving feature.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    public Dictionary<Vector2,Transform> cells = new Dictionary<Vector2, Transform>();
+    public Vector2 enterPoint;
+    public Vector2 exitPoint;
+
+    public static Board current;
+
+	void Awake () {
+        current = this;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            var coords = new Vector2(Mathf.Round(child.transform.position.x - 0.5f),
+                Mathf.Round(child.transform.position.y - 0.5f));
+
+            child.transform.position = new Vector2(coords.x + 0.5f, coords.y + 0.5f);
+            cells.Add(coords, child);
+
+            if (child.tag == "Enter") enterPoint = coords;
+            if (child.tag == "Exit") exitPoint = coords;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Board board;
+    public Vector2 coords;
+
+    void Start () {
+        board = Board.current;
+        MoveTo(board.enterPoint);
+	}
+
+    private void MoveTo(Vector2 enterPoint)
+    {
+        if (!board.cells.ContainsKey(enterPoint))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            return;
+        }
+
+        var nextTile = board.cells[enterPoint];
+        if (nextTile.tag == "Unpassable") return;
+
+        transform.position = new Vector3(enterPoint.x + 0.5f, enterPoint.y + 0.5f, -1);
+        coords = enterPoint;
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            MoveTo(coords + Vector2.down);
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            MoveTo(coords + Vector2.up);
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            MoveTo(coords + Vector2.left);
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            MoveTo(coords + Vector2.right);
+	}
+}
