f29523096381f49569cd0943fb032b539963d49d
Mehael <mehael@mail.ru>
2017-07-29 18:15:22 +1000

    Add spikes

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -23,18 +23,41 @@ public class Tile : MonoBehaviour {
 
     public bool PressedSignUp()
     {
-        if (tag == "Weak")
+        if (tag == "Weak" || tag == "Trap")
             return true;
 
         return false;
     }
 
+    public Sprite activeTrapState;
+    public Sprite passiveTrapState;
+    public int TurnsToBecomePassive = 0;
+
     public bool StayOneMoreTurn()
     {
+        if (tag == "Trap" && sprite.sprite == passiveTrapState)
+        {
+            tag = "Death";
+            TurnsToBecomePassive = 2;
+            sprite.sprite = activeTrapState;
+            return true;
+        }
+
+        if (tag == "Death")
+        {
+            TurnsToBecomePassive--;
+            if (TurnsToBecomePassive == 0)
+            {
+                tag = "Trap";
+                sprite.sprite = passiveTrapState;
+            }
+            else return true;
+        }
+
         if (tag == "Weak")
         {
             tag = "Death";
-            sprite.enabled = false;          
+            sprite.enabled = false;
         }
 
         return false;
